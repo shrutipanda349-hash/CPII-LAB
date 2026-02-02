293aedb3a6fd5a831cc740f944ecd7040153d16f
Shruti Panda <shrutipanda349@gmail.com>
2026-02-02 16:46:35 +0530

    Add files via upload
    ASSIGNMENT OF LAB 4

---
+++ lab4_3.c
@@ -0,0 +1,15 @@
+//Write a program to reverse a given string and display it//
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char string[100];
+	printf("Enter the string data:");
+	gets(string);
+	strrev(string);
+	puts(string);
+     return 0;
+	 }
+
+
+
---
+++ lab4_4.c
@@ -0,0 +1,21 @@
+//How can you implement a program to count the number of vowels in a string?//
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char s[40];
+	int i,d=0;
+	printf("Enter the string data:");
+	gets(s);
+	for(i=0;s[i]!=0;i++)
+{
+		if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+		d++;
+}
+	
+	
+	printf("num of vowels=%d",d);
+}
+
+
+
