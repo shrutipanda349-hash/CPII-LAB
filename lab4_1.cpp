021f469bbf4f41550a8bd8425680b943eb4e29e9
Shruti Panda <shrutipanda349@gmail.com>
2026-02-02 16:11:49 +0530

    Add files via upload
    assignment of lab 4

---
+++ lab4_1.cpp
@@ -0,0 +1,10 @@
+//Write a program to take a string as input and print it//
+#include<stdio.h>
+int main()
+{
+char input_string[100];
+printf("Enter the string:");
+gets(input_string);
+printf("%s",input_string);
+return 0;
+}
---
+++ lab4_2.cpp
@@ -0,0 +1,14 @@
+//Create a program to input a string and display each character on a new line.//
+#include<stdio.h>
+#include<string.h>
+int main()
+{ char string[50];
+int i=0;
+printf("Enter the string data:");
+gets(string);
+for(i=0;string[i]!=0;i++)
+{
+printf("%c\n",string[i]);
+}
+
+}
