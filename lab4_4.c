9cd380ffa9b62d2d257c72a5ccf9eaddb20278b2
Shruti Panda <shrutipanda349@gmail.com>
2026-02-02 16:51:00 +0530

    Update lab4_4.c

--- lab4_4.c
+++ lab4_4.c
@@ -9,13 +9,14 @@ void main()
 	gets(s);
 	for(i=0;s[i]!=0;i++)
 {
-		if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+		if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
 		d++;
 }
 	
 	
 	printf("num of vowels=%d",d);
 }
+
 
 
 
